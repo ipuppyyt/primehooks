53fb4348837e49606eb646f5d39c2062f06ca983
Dhanush S <dhanush6102002@gmail.com>
2025-10-04 17:24:08 +0530

    feat: add linting

---
+++ eslint.config.ts
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default tseslint.config(
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    { ignores: ['dist/', 'deploy/', '.github/', 'docs/', 'web/', '**/node_modules/'] },
+    {
+        files: ['src/hooks**/*.{ts}'],
+        plugins: {
+            'react-hooks': reactHooks,
+        },
+        rules: {
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+        },
+        ignores: ['dist'],
+    }
+);
